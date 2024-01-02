602f8699c8f7714ee3ae23df9c9e53172dc8716a
Vijay Mehra <129831646+MehrajiClasses@users.noreply.github.com>
2024-01-02 21:30:54 +0530

    Update index.js
    some changes

--- index.js
+++ index.js
@@ -9,18 +9,18 @@
 //      });
 
 // //showing same output if any button is clicked
-// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-// for(var i = 0; i<numberOfDrumButtons ; i++){
+// let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// for(let i = 0; i<numberOfDrumButtons ; i++){
 //     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 //         alert("I got Clicked!");
 //      });
 // }
 
 //playing audio when button clicked
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-for(var i = 0; i < numberOfDrumButtons; i++){
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(let i = 0; i < numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-       var buttonInnerHTML = this.innerHTML;
+       let buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
 
         buttonAnimation(buttonInnerHTML);
@@ -37,37 +37,37 @@ document.addEventListener("keydown",function(event){
 function makeSound(key){
     switch(key){
         case "w":
-            var crash = new Audio("./sounds/crash.mp3");
+            let crash = new Audio("./sounds/crash.mp3");
              crash.play();
         break;
 
         case "a":
-            var kick = new Audio("./sounds/kick-bass.mp3");
+            let kick = new Audio("./sounds/kick-bass.mp3");
              kick.play();
         break;
 
         case "s":
-            var snare = new Audio("./sounds/snare.mp3");
+            let snare = new Audio("./sounds/snare.mp3");
              snare.play();
         break;
 
         case "d":
-            var tom1 = new Audio("./sounds/tom-1.mp3");
+            let tom1 = new Audio("./sounds/tom-1.mp3");
              tom1.play();
         break;
 
         case "j":
-            var tom2 = new Audio("./sounds/tom-2.mp3");
+            let tom2 = new Audio("./sounds/tom-2.mp3");
              tom2.play();
         break;
 
         case "k":
-            var tom3 = new Audio("./sounds/tom-3.mp3");
+            let tom3 = new Audio("./sounds/tom-3.mp3");
              tom3.play();
         break;
 
         case "l":
-            var tom4 = new Audio("./sounds/tom-4.mp3");
+            let tom4 = new Audio("./sounds/tom-4.mp3");
              tom4.play();
         break;
 
@@ -79,7 +79,7 @@ function makeSound(key){
 }
 //applying Animation
 function buttonAnimation(currentKey){
-    var activeButton = document.querySelector("."+currentKey);
+    let activeButton = document.querySelector("."+currentKey);
     activeButton.classList.add("pressed");
 
     setTimeout(function(){
